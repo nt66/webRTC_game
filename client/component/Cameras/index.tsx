8c34157eb8e5f6ffc36c77fad9701886efa390f7
h1692 <tc_hualingfeng@163.com>
2021-08-15 10:27:16 +0800

    feat:获取本地音视频流

--- client/component/Cameras/index.tsx
+++ client/component/Cameras/index.tsx
@@ -7,18 +7,18 @@ const Cameras = () => {
   const videoMine = useRef(null)
   const videoStudent = useRef(null)
 
+  // getlocalmedia
   const  getUserMedia =()=> {
     // 兼容浏览器的getUserMedia写法
     let mineVideo = videoMine.current
-
-    let getUserMedia = (navigator.getUserMedia)
+    let getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia )
     // 获取本地的媒体流，并绑定到一个audio标签上输出，并且发送这个媒体流给其他客户端
     return new Promise((resolve, reject) => {
       getUserMedia.call(navigator, {
         'audio': true,
         'video': true
       }, (stream) => {
-        // 绑定本地媒体流到audio标签用于输出
+        // 绑定本地媒体流到video标签用于输出
         mineVideo.srcObject = stream
         resolve()
       }, function (error) {
@@ -29,45 +29,10 @@ const Cameras = () => {
     })
   }
 
-  // const getPeerConnection =()=> {
-  //   let audioBox = this.$refs['audio-box']
-  //   let iceServer = {
-  //     'iceServers': [
-  //     ]
-  //   }
-  //   // 兼容浏览器的PeerConnection写法
-  //   let PeerConnection = (window.RTCPeerConnection ||
-  //                 window.webkitRTCPeerConnection ||
-  //                 window.mozRTCPeerConnection)
-  //   // 创建
-  //   let peer = new PeerConnection(iceServer)
-  //   // 向PeerConnection中加入需要发送的流
-  //   peer.addStream(this.localStream)
+  // peerConnection
+  const getPeerConnection =()=> {
 
-  //   // 如果检测到媒体流连接到本地，将其绑定到一个audio标签上输出
-  //   peer.onaddstream = function (event) {
-  //     // console.log('event-stream', event);
-  //     let audios = document.querySelector('#' + v.account)
-  //     if (audios) {
-  //       audios.srcObject = event.stream
-  //     } else {
-  //       let audio = document.createElement('audio')
-  //       audio.controls = true
-  //       audio.autoplay = 'autoplay'
-  //       audio.srcObject = event.stream
-  //       audio.id = v.account
-  //       audioBox.append(audio)
-  //     }
-  //   }
-  //   // 发送ICE候选到其他客户端
-  //   peer.onicecandidate = (event) => {
-  //     if (event.candidate) {
-  //       socket.emit('__ice_candidate', {'candidate': event.candidate, roomid: this.$route.params.roomid, account: v.account})
-  //     }
-  //   }
-  //   console.log('v.account', v.account)
-  //   this.peerList[v.account] = peer
-  // }
+  }
 
   useEffect(()=>{
     getUserMedia().then(()=>{
@@ -75,19 +40,17 @@ const Cameras = () => {
     });
 
     socket.on('join',()=>{
-      console.log('joina....')
-      // getPeerConnection()
-      // console.log('joinb....',b)
+      getPeerConnection()
     })
   })
 
   return (
     <div className={style.camerasMain}>
       <div className={style.camera}> 
-        <video className={style.videoMine} ref={videoMine}></video>
+        <video className={style.videoMine} ref={videoMine}  autoplay="autoplay"></video>
       </div>
       <div className={style.camera} style={{ marginLeft: '100px' }}>
-        <video className={style.videoStudent} ref={videoStudent}></video>
+        <video className={style.videoStudent} ref={videoStudent}  autoplay="autoplay"></video>
       </div>
     </div>
   )
