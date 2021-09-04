3025d2104190aa945743071414e245168e0b0867
hualingfeng <tc_hualingfeng@163.com>
2021-09-05 00:13:07 +0800

    feat: add forward back pen

--- client/component/Viewer/index.tsx
+++ client/component/Viewer/index.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Button } from 'antd'
+import { StepForwardOutlined, StepBackwardOutlined,EditOutlined } from '@ant-design/icons'
 import { connect } from 'dva'
 import styles from './index.less'
 import imgUrl from '@/assets/lesson/01/p1.png';
@@ -7,9 +9,15 @@ const Viewer = () => {
   // const imgUrl = await require("@/assets/lesson/01/p1.png")
   return (
     <div className={styles.viewer}>
-      <div>
+      <div className={styles.pptView}>
         <img src={imgUrl} alt="img" />
       </div>
+      <div className={styles.tool} >
+        <Button type="primary" shape="circle"  icon={<StepForwardOutlined />} style={{marginBottom:'30px'}}  ></Button>
+        <Button type="primary" shape="circle"  icon={<StepBackwardOutlined />} style={{marginBottom:'30px'}} ></Button>
+        <Button type="primary" shape="circle"  icon={<EditOutlined />} style={{marginBottom:'30px'}} ></Button>
+      </div>
+
     </div>
   )
 }
